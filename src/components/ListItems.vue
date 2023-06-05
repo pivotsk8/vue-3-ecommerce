49421a67fa65689e29bfed0b8ff1d0e3884cf014
piiivot <the_pivotsk8@hotmail.com>
2023-06-05 09:42:47 -0500

    feat:loading

--- src/components/ListItems.vue
+++ src/components/ListItems.vue
@@ -13,12 +13,13 @@ const key = (id:any) => id++
 </script>
 
 <template>  
-  <div>
-    <VRow>
-        <VCol v-for="product in products" :key="key(product.id)" cols="4">
+  <div v-if="products.length===0" class="d-flex justify-center align-center h-100">
+    <VProgressCircular  indeterminate :size="50"/>
+  </div>
+    <VRow v-else>
+        <VCol  v-for="product in products" :key="key(product.id)" cols="4">
           <ItemCard 
           :products="product" />
         </VCol>
       </VRow>
-     </div>
 </template>
