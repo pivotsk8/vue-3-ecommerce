7b2c349aad3e0c49e50f4ff945066f529cdc7ffb
piiivot <the_pivotsk8@hotmail.com>
2023-06-05 11:15:58 -0500

    feat:Se adapto el mensaje de whatsAp

--- src/components/cart/SummaryCard.vue
+++ src/components/cart/SummaryCard.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
 import { useCartStore } from '@/stores/cart';
+import { storeToRefs } from 'pinia';
 
 // store
-const carStore =useCartStore()
-const {totalAmount} = carStore 
+const carStore = useCartStore()
+const { totalAmount } = carStore
+const { whatsAppLink } = storeToRefs(carStore)
 </script>
 <template>
     <div>
         <VCard>
             <VCardTitle>
-               Resumen de la compra 
+                Resumen de la compra
             </VCardTitle>
 
             <VCardSubtitle>
-                Total a pagar: {{totalAmount()}}
+                Total a pagar: {{ totalAmount() }}
             </VCardSubtitle>
 
             <VCardText>
-                <VBtn variant="tonal" color="orange-lighten-2">
+                <VBtn :href="whatsAppLink" variant="tonal" target="_blank" color="orange-lighten-2">
                     Realizar pedido
                 </VBtn>
             </VCardText>
--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,63 +1,90 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
-import type {CartDetail, Products} from '../model/tipos'
+import { computed, ref } from "vue";
+import { defineStore } from "pinia";
+import type { CartDetail, Products } from "../model/tipos";
+import { useLocalStorage } from "@vueuse/core";
 
+export const useCartStore = defineStore("cart", () => {
+  const details = ref(useLocalStorage<CartDetail[]>("carDetails", []));
 
-export const useCartStore = defineStore('cart', () => {
-  const details = ref <Array<CartDetail>>([])
+  //getters
+  const sendWatsAppMessage = () => {
+    let message = "hola quiero realizar la siguiente compra:\n\n";
+    details.value.forEach((d) => {
+      message += `Producto: ${d.product.name} \n`;
+      message += `Cantidad: ${d.amount}\n`;
+      message += `SubTotal: $${d.amount * d.product.price}\n`;
+      message += "____________________________________ \n";
+    });
 
-// action
+    message += `\n Total a pagar: ${totalAmount()} \n\n`;
+    message += "Muchas gracias! 🙂";
+    console.log(message);
+
+    return encodeURI(message);
+  };
+  const whatsAppLink = computed(
+    () =>
+      `https://api.whatsapp.com/send/?phone=573227265734&text=${sendWatsAppMessage()}`
+  );
+
+  // action
   const addProduct = (product: Products) => {
-    const detailFound = details.value.find(detail => detail.product.id === product.id)
-    detailFound 
-    ? detailFound.amount++
-    :details.value.push({
-      product,
-      amount: 1
-    })
-  }
+    const detailFound = details.value.find(
+      (detail) => detail.product.id === product.id
+    );
+    detailFound
+      ? detailFound.amount++
+      : details.value.push({
+          product,
+          amount: 1,
+        });
+  };
 
-  const deleteProduct = (productId: number)=>{
-   const index =  details.value.findIndex(detail => detail.product.id === productId)
-   details.value.splice(index, 1)
-    
-  }
-  
+  const deleteProduct = (productId: number) => {
+    const index = details.value.findIndex(
+      (detail) => detail.product.id === productId
+    );
+    details.value.splice(index, 1);
+  };
+
   const itemCount = () => {
     let count = 0;
-    details.value.forEach(element => {
+    details.value.forEach((element) => {
       count += element.amount;
     });
-    return count
-  }
-  
-  const increment = (productId:number) => {
-  const detailFound = details.value.find(detail => detail.product.id === productId)
-  detailFound ? detailFound.amount++ : null
-}
+    return count;
+  };
 
-const decrement = (productId:number) => {
-  const detailFound = details.value.find(detail => detail.product.id === productId)
-  detailFound ? detailFound.amount-- : null
-  detailFound?.amount === 0 ? deleteProduct(productId) : null
+  const increment = (productId: number) => {
+    const detailFound = details.value.find(
+      (detail) => detail.product.id === productId
+    );
+    detailFound ? detailFound.amount++ : null;
+  };
 
-}
-
-const totalAmount = () => {
-  let sum = 0;
-  details.value.forEach(element => {
-    sum += element.amount * element.product.price;
-  });
-  return sum
-}
+  const decrement = (productId: number) => {
+    const detailFound = details.value.find(
+      (detail) => detail.product.id === productId
+    );
+    detailFound ? detailFound.amount-- : null;
+    detailFound?.amount === 0 ? deleteProduct(productId) : null;
+  };
+  const totalAmount = () => {
+    let sum = 0;
+    details.value.forEach((element) => {
+      sum += element.amount * element.product.price;
+    });
+    return sum;
+  };
 
   return {
-    details, 
-    addProduct, 
-    itemCount, 
-    increment, 
-    decrement, 
+    details,
+    whatsAppLink,
+    addProduct,
+    itemCount,
+    increment,
+    decrement,
     deleteProduct,
-    totalAmount
-  }
-})
+    totalAmount,
+  };
+});
